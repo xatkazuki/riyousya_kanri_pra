895ed6a5337a15f3481c88b1eec70c0d7f5aa335
takabayashi <you@example.com>
2019-09-17 11:46:40 +0900

    スタッフ検索の新たなFormTypeを作成

---
+++ src/Form/YadoStaffSerchType.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xearts
+ * Date: 2019/09/13
+ * Time: 18:42
+ */
+
+namespace App\Form;
+
+
+use App\Entity\YadoStaff;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class YadoStaffType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $optiobns
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options){
+        $builder
+            ->add('famillyName')
+            ->add('firstNmae')
+            ->add('position',ChoiceType::class,[
+                'choices' => [
+                    '正社員'=>'正社員',
+                    '契約社員'=>'契約社員',
+                    'パート'=>'パート',
+                    '学生アルバイト'=>'学生アルバイト',
+                ]])
+            ->add('nyuusya_at')
+            ->add('taisya_at')
+            ->add('created_at')
+            ->add('updated_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => YadoStaff::class,
+        ]);
+    }
+
+}
